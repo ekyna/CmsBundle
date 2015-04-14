e5194dcab16bb763300eed52b0a28797c6100543
Etienne Dauvergne <contact@ekyna.com>
2015-04-14 09:19:18 +0200

    File action

--- Controller/CmsController.php
+++ Controller/CmsController.php
@@ -3,6 +3,7 @@
 namespace Ekyna\Bundle\CmsBundle\Controller;
 
 use Ekyna\Bundle\CoreBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -84,5 +85,26 @@ class CmsController extends Controller
             ->render('EkynaCmsBundle:Cms:flashes.html.twig')
             ->setPrivate()
         ;
+    }
+
+    /**
+     * Serve local file.
+     *
+     * @param Request $request
+     * @return BinaryFileResponse
+     * @throws NotFoundHttpException
+     */
+    public function fileAction(Request $request)
+    {
+        $key = $request->attributes->get('key');
+        if (0 < strlen($key)) {
+            $gfs = $this->get('gaufrette.local_file_filesystem');
+            if ($gfs->has($key)) {
+                $file = 'media://local_file/'.$key;
+                return new BinaryFileResponse($file);
+            }
+        }
+
+        throw new NotFoundHttpException('File not found');
     }
 }
