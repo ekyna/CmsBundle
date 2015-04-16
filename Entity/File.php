cb613854a8f32224353294ea2ab13c26ef0a317f
Etienne Dauvergne <contact@ekyna.com>
2015-04-16 14:04:13 +0200

    File as tagged entity

--- Entity/File.php
+++ Entity/File.php
@@ -3,14 +3,18 @@
 namespace Ekyna\Bundle\CmsBundle\Entity;
 
 use Ekyna\Bundle\CoreBundle\Entity\AbstractFile;
+use Ekyna\Bundle\CoreBundle\Model\TaggedEntityInterface;
+use Ekyna\Bundle\CoreBundle\Model\TaggedEntityTrait;
 
 /**
  * Class File
  * @package Ekyna\Bundle\CmsBundle\Entity
  * @author Étienne Dauvergne <contact@ekyna.com>
  */
-class File extends AbstractFile
+class File extends AbstractFile implements TaggedEntityInterface
 {
+    use TaggedEntityTrait;
+
     /**
      * @var \DateTime
      */
@@ -37,5 +41,13 @@ class File extends AbstractFile
     {
         $this->createdAt = $createdAt;
         return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getEntityTagPrefix()
+    {
+        return 'ekyna_cms.file';
     }
 }
