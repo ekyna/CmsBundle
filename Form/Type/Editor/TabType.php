ff86ee0c468d65b30aa5c9667ac356ae09183ce8
Etienne Dauvergne <contact@ekyna.com>
2019-04-23 17:07:19 +0200

    [Cms] Fixed editor tabs widget.

--- Form/Type/Editor/TabType.php
+++ Form/Type/Editor/TabType.php
@@ -5,9 +5,12 @@ namespace Ekyna\Bundle\CmsBundle\Form\Type\Editor;
 use A2lix\TranslationFormBundle\Form\Type\TranslationsFormsType;
 use Ekyna\Bundle\CmsBundle\Editor\Plugin\Block\Model\Tab;
 use Ekyna\Bundle\CoreBundle\Form\Type\CollectionPositionType;
+use Ekyna\Component\Resource\Locale\LocaleProviderInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
@@ -17,6 +20,22 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
  */
 class TabType extends AbstractType
 {
+    /**
+     * @var LocaleProviderInterface
+     */
+    private $localeProvider;
+
+
+    /**
+     * Constructor.
+     *
+     * @param LocaleProviderInterface $localeProvider
+     */
+    public function __construct(LocaleProviderInterface $localeProvider)
+    {
+        $this->localeProvider = $localeProvider;
+    }
+
     /**
      * @inheritdoc
      */
@@ -32,7 +51,18 @@ class TabType extends AbstractType
                 'label'    => 'Anchor', // TODO
                 'required' => false,
             ])
-            ->add('position', CollectionPositionType::class);
+            ->add('position', CollectionPositionType::class)
+            ->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
+                $data = $event->getData();
+
+                if (!$data instanceof Tab) {
+                    return;
+                }
+
+                $data
+                    ->setCurrentLocale($this->localeProvider->getCurrentLocale())
+                    ->setFallbackLocale($this->localeProvider->getFallbackLocale());
+            });
     }
 
     /**
