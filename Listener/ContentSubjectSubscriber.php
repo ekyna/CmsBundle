54c7ade4c862c882d53892d5f0fb1cb60c310e03
Étienne Dauvergne <contact@ekyna.com>
2023-05-12 15:46:07 +0200

    [Cms] Allow to delete content.

--- Listener/ContentSubjectSubscriber.php
+++ Listener/ContentSubjectSubscriber.php
@@ -43,7 +43,7 @@ class ContentSubjectSubscriber
                 [
                     'name'                 => 'content_id',
                     'referencedColumnName' => 'id',
-                    'onDelete'             => 'RESTRICT',
+                    'onDelete'             => 'SET NULL',
                     'nullable'             => true,
                 ],
             ],
